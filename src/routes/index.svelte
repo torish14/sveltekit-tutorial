7066515e15f58be37dfec44dc52fc5d85c443d8a
torish14 <nanamin.104@icloud.com>
2022-03-28 19:29:45 +0900

    ✨ feat: update title ボタンをクリックしたら、タイトルが更新される

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,15 @@
 <script>
 let title = 'Ninja Gaming Guid'
+
+const updateTitle = () => {
+	title = 'complete different title'
+}
 </script>
 
 <div class="index">
 	<h2 class="title">{title}</h2>
 	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione distinctio sit harum labore molestias facere officiis voluptatibus sapiente autem placeat nihil repellat ad sed eligendi possimus, aliquam doloribus porro repellendus.</p>
+	<button class="button" on:click={updateTitle}>update title</button>
 </div>
 
 <style>
