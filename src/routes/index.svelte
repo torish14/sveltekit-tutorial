237cda08e78fe8564df800195a977d5bc342893b
torish14 <nanamin.104@icloud.com>
2022-03-28 19:20:55 +0900

    ✨ feat: サンプルテキストを変更

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,15 @@
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>サンプルです</p>
+<div class="index">
+	<h2>some title</h2>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione distinctio sit harum labore molestias facere officiis voluptatibus sapiente autem placeat nihil repellat ad sed eligendi possimus, aliquam doloribus porro repellendus.</p>
+</div>
 
 <style>
-
+.index {
+	text-align: center;
+	display: block;
+	margin: 20px auto;
+}
 </style>
