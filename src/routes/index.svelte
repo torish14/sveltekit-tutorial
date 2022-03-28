f6f2d7e69df860e978aa41861c19c6a212c911d9
torish14 <nanamin.104@icloud.com>
2022-03-28 19:22:12 +0900

    ✨ feat: 変数を使用してタイトルを変更

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script>
-
+let title = 'Ninja Gaming Guid'
 </script>
 
 <div class="index">
-	<h2>some title</h2>
+	<h2>{title}</h2>
 	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione distinctio sit harum labore molestias facere officiis voluptatibus sapiente autem placeat nihil repellat ad sed eligendi possimus, aliquam doloribus porro repellendus.</p>
 </div>
 
